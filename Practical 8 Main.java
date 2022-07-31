f5af9471ced07c097f185122436ad2484bb13b64
imhetpatel16 <96860998+imhetpatel16@users.noreply.github.com>
2022-07-31 17:38:26 +0530

    Create Practical 8 Main.java

---
+++ Practical 8 Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+
+    public class practical_8_main {
+
+        public static void main(String[] args) {
+
+            practical_8 ww = new practical_8();
+            ww.exam();
+            System.out.println("Created By:- Het Patel \n" + "ID:- 21CE091");
+
+        }
+    }
+
