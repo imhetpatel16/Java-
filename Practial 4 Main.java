775e625297538fa50d6f1cd908a3c2c61f1f4469
imhetpatel16 <96860998+imhetpatel16@users.noreply.github.com>
2022-07-31 16:01:49 +0530

    Create Practial 4 Main.java

---
+++ Practial 4 Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ass_4_main {
+    public static void main(String[] args) {
+
+        Scanner scs = new Scanner(System.in);
+
+        System.out.print("Enter the Size of Array -> ");
+        int aa = scs.nextInt();
+        Ass_4 as = new Ass_4(aa);
+        System.out.println("Created By:- Rushit Rabadiya \n" + "ID:- 21CE111");
+    }
+}
