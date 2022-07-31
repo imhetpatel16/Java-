e9e2c0b434039e82d643bda34a8e263f46695567
imhetpatel16 <96860998+imhetpatel16@users.noreply.github.com>
2022-07-31 16:13:23 +0530

    Update Practial 4 Main.java

--- Practial 4 Main.java
+++ Practial 4 Main.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-public class Ass_4_main {
+public class practical_4_main {
     public static void main(String[] args) {
 
         Scanner scs = new Scanner(System.in);
 
         System.out.print("Enter the Size of Array -> ");
         int aa = scs.nextInt();
-        Ass_4 as = new Ass_4(aa);
+        pra_4 as = new pra_4(aa);
         System.out.println("Created By:- Het Patel \n" + "ID:- 21CE091");
     }
 }
